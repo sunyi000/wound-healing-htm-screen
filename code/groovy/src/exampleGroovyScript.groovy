94a24e155eef5b94bb8a48101c74872c771adbf9
Christian Tischer <christian.tischer@embl.de>
2021-12-07 13:01:19 +0100

    Add groovy scripting

---
+++ code/groovy/src/exampleGroovyScript.groovy
@@ -0,0 +1,30 @@
+import ij.*
+
+/**
+ *
+ * Repository:
+ *
+ *
+ * Creation:
+ * Date: Month 2021
+ * By: firstname.lastname@embl.de
+ * For: firstname.lastname@embl.de
+ *
+ * Requirements
+ * - ImageJ
+ *
+ * inputs:
+ * - input number 1
+ * - input number 2
+ * - ......
+ *
+ * outputs:
+ * - output number 1
+ * - output number 2
+ * - ....
+ *
+ */
+
+
+def sampleImage = IJ.openImage("http://imagej.nih.gov/ij/images/blobs.gif")
+sampleImage.show()
