665095243e2b9d264f17a166583c2367c21c7c0f
Christian Tischer <christian.tischer@embl.de>
2023-09-21 15:48:10 +0200

    Improve documentation

--- code/nextflow/work/2a/41c784333ddd77dc852a0fb784fafc/.command.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -ue
-echo "Processing dataset:" A3ROI2_Slow
-echo "Contained files:" A3ROI2_Slow_0010.tif A3ROI2_Slow_0004.tif A3ROI2_Slow_0005.tif A3ROI2_Slow_0011.tif A3ROI2_Slow_0007.tif A3ROI2_Slow_0006.tif A3ROI2_Slow_0002.tif A3ROI2_Slow_0003.tif A3ROI2_Slow_0001.tif A3ROI2_Slow_0000.tif A3ROI2_Slow_0008.tif A3ROI2_Slow_0009.tif
-echo "Fiji script:" /Users/tischer/Documents/wound-healing-htm-screen/code/groovy/src/main/measureWoundClosing.groovy
-"/Users/tischer/Desktop/Fiji/Fiji.app/Contents/MacOS/ImageJ-macosx" --ij2 --headless --run "/Users/tischer/Documents/wound-healing-htm-screen/code/groovy/src/main/measureWoundClosing.groovy" 'inputDir="../../data/input",datasetId="A3ROI2_Slow",threshold="25",outDirName="output",headless="true",saveResults="true"'
-echo "Done: " A3ROI2_Slow
--- code/nextflow/work/5c/8ae14399fbc1b5791ccabac89fac81/.command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -ue
-echo "Processing dataset:" C4ROI1_Fast
-echo "Contained files:" C4ROI1_Fast_0009.tif C4ROI1_Fast_0008.tif C4ROI1_Fast_0005.tif C4ROI1_Fast_0011.tif C4ROI1_Fast_0010.tif C4ROI1_Fast_0004.tif C4ROI1_Fast_0006.tif C4ROI1_Fast_0007.tif C4ROI1_Fast_0003.tif C4ROI1_Fast_0002.tif C4ROI1_Fast_0000.tif C4ROI1_Fast_0001.tif
-"fiji" --ij2 --headless --run "../groovy/src/main/measureWoundClosing.groovy" 'inputDir="../../data/input",datasetId="C4ROI1_Fast",threshold="25",outDirName="output--fiji",headless="true",saveResults="true"'
